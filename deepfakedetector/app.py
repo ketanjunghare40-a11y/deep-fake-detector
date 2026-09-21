2badf59fce0b193cb249e22d1631210791b2f93e
ketanjunghare40-a11y <ketanjunghare40@gmail.com>
2026-09-21 16:09:07 +0530

    Initial commit

---
+++ deepfakedetector/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "static/uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    prediction = None
+    confidence = None
+    image = None
+
+    if request.method == "POST":
+
+        file = request.files["image"]
+
+        if file:
+
+            path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+
+            file.save(path)
+
+            # Demo result (not real AI)
+            prediction = "Real Image"
+            confidence = "95"
+
+            image = path
+
+    return render_template(
+        "index.html",
+        prediction=prediction,
+        confidence=confidence,
+        image=image
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
